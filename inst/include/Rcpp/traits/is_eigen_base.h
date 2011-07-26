3a59545c69cf23b7703c00282555bbcb8baff6af
Unknown author <unknown@unknown>
2011-07-26 00:00:00 +0000

    version 0.9.6

--- inst/include/Rcpp/traits/is_eigen_base.h
+++ inst/include/Rcpp/traits/is_eigen_base.h
@@ -1,8 +1,8 @@
 // -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; tab-width: 4 -*-
 //
-// config.h: Rcpp R/C++ interface class library -- Rcpp configuration of optional features
+// is_eigen_base.h: Rcpp R/C++ interface class library -- 
 //
-// Copyright (C) 2010 - 2011 Dirk Eddelbuettel and Romain Francois
+// Copyright (C) 2011 Doug Bates, Dirk Eddelbuettel and Romain Francois
 //
 // This file is part of Rcpp.
 //
@@ -19,15 +19,29 @@
 // You should have received a copy of the GNU General Public License
 // along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
-#ifndef RCPP__CONFIG_H
-#define RCPP__CONFIG_H
+#ifndef Rcpp__traits__is_eigen_base_h
+#define Rcpp__traits__is_eigen_base_h
 
-#ifdef __GNUC__
-#define RCPP_HAS_DEMANGLING
+namespace Rcpp{
+namespace traits{
+
+	template<typename T>
+	class _is_eigen_helper : __sfinae_types {
+      template<typename U> struct _Wrap_type { };
+
+      template<typename U>
+        static __one __test(_Wrap_type<typename U::StorageKind>*);
+
+      template<typename U>
+        static __two __test(...);
+
+    public:
+      static const bool value = sizeof(__test<T>(0)) == 1;
+    };
+  
+  template<typename T> struct is_eigen_base : 
+  	integral_constant<bool, _is_eigen_helper<T>::value >{ };
+
+}
+}
 #endif
-
-#define Rcpp_Version(v,p,s) (((v) * 65536) + ((p) * 256) + (s))
-#define RCPP_VERSION Rcpp_Version(0,9,6)
-
-#endif
-
