1bd9c7c3cdfb0ec04c6ba375dcef9438472407e3
Dirk Eddelbuettel and Romain Francois <RomainAndDirk@r-enthusiasts.com>
2011-07-27 13:41:02 +0000

    version 0.9.6

--- inst/include/Rcpp/internal/wrap.h
+++ inst/include/Rcpp/internal/wrap.h
@@ -29,8 +29,13 @@
 // don't include it directly
 
 namespace Rcpp{
-
+	namespace RcppEigen{
+		template <typename T> SEXP eigen_wrap( const T& object ) ;
+	}
+	
 template <typename T> SEXP wrap( const T& object ) ;
+
+template <typename T> class CustomImporter ;
 
 namespace internal{
 	
@@ -644,13 +649,24 @@ inline SEXP wrap_dispatch( const T& object, ::Rcpp::traits::wrap_type_primitive_
 	return primitive_wrap( object ) ;
 }
 
+template <typename T>
+inline SEXP wrap_dispatch_eigen( const T& object, ::Rcpp::traits::false_type){
+	return wrap_dispatch_unknown( object, typename ::Rcpp::traits::is_convertible<T,SEXP>::type() ) ;
+}
+
+template <typename T>
+inline SEXP wrap_dispatch_eigen( const T& object, ::Rcpp::traits::true_type){
+	return ::Rcpp::RcppEigen::eigen_wrap( object ) ;
+}
+
+
 /**
  * called when T is wrap_type_unknown_tag and is not an Importer class
  * The next step is to try implicit conversion to SEXP
  */
 template <typename T> 
 inline SEXP wrap_dispatch_unknown_importable( const T& object, ::Rcpp::traits::false_type){
-	return wrap_dispatch_unknown( object, typename ::Rcpp::traits::is_convertible<T,SEXP>::type() ) ;
+	return wrap_dispatch_eigen( object, typename traits::is_eigen_base<T>::type() ) ;
 }
 
 /**
@@ -660,8 +676,7 @@ template <typename T>
 inline SEXP wrap_dispatch_unknown_importable( const T& object, ::Rcpp::traits::true_type){
 	return wrap_dispatch_importer<T,typename T::r_import_type>( object ) ;
 }
- 
- 
+
 /** 
  * This is called by wrap when the wrap_type_traits is wrap_type_unknown_tag
  * 
